b1ad26b727d3e15e1431bd2a52c3c6e0e2a321fb
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2025-12-03 02:42:06 +0000

    feat: adicionando ferramenta para padrao de codificacao

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,28 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["./sistema-de-vendas/tsconfig.app.json", "./sistema-de-vendas/tsconfig.spec.json"],
-    tsconfigRootDir: __dirname
+    project: [
+      "./sistema-de-vendas/tsconfig.app.json",
+      "./sistema-de-vendas/tsconfig.spec.json",
+    ],
+    tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint", "deprecation"],
+  plugins: ["@typescript-eslint", "deprecation", "import"],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "airbnb-base",
   ],
   rules: {
-    // ⚠️ Detectar código depreciado
     "deprecation/deprecation": "error",
 
-    // 🚫 Desativar outras regras "chatas"
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "prefer-const": "off"
-  }
+    "prefer-const": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
 };
