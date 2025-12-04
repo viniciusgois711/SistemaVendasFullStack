2e656cd02c25e55a77ea561f44373cead85ee362
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2025-12-04 01:58:58 +0000

    fix: corrigindo codigo duplicado

--- sistema-de-vendas/src/app/services/produtos.service.ts
+++ sistema-de-vendas/src/app/services/produtos.service.ts
@@ -25,21 +25,4 @@ export class ProdutosService {
   deleteProdutosApi(produtos: any): Observable<any> {
     return this.http.delete(`${this.urlApiProdutos}/${produtos.id}`);
   }
-
-  // Código duplicado de clientes.service.ts - exemplo de duplicação
-  getClientesApi(path = ''): Observable<any> {
-    return this.http.get<any>('http://localhost:3000/clientes' + path);
-  }
-
-  postClienteApi(cliente: any): Observable<any> {
-    return this.http.post<any>('http://localhost:3000/clientes', cliente);
-  }
-
-  putClienteApi(cliente: any): Observable<any> {
-    return this.http.put<any>(`${'http://localhost:3000/clientes'}/${cliente.id}`, cliente);
-  }
-
-  deleteClienteApi(cliente: any): Observable<any> {
-    return this.http.delete<any>(`${'http://localhost:3000/clientes'}/${cliente.id}`);
-  }
 }
